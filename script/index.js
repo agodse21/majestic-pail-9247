8d4912ac7336a60031f1dd73b11320bd9d2aa63d
sanjay gupta <guptaji30749@gmail.com>
2022-07-21 20:21:43 +0530

    add function

--- script/index.js
+++ script/index.js
@@ -2,22 +2,23 @@ function getdata(e) {
 
     e.preventDefault()
 
-    let form = document.getElementById("form")
+    let form_data = document.getElementById("data")
 
-    let name = form.name.value
+    let name = form_data.name.value
 
-    let last = form.Last.value
+    let last = form_data.Last.value
 
-    let email = form.email.value
+    let email = form_data.email.value
 
-    let company = form.company.value
+    let company = form_data.company.value
 
-    let job = form.job.value
+    let job = form_data.job.value
 
-    let phn = form.phn.value
+    let phn = form_data.phn.value
 
-    let india = form.india.value
+    let india = form_data.india.value
 
 
-    console.log(name, last, email, company, job, phn, india)
+    // console.log(name, last, email, company, job, phn, india)
+    alert("Your enter successfully !")
 }
