2ffc2dc52adacce061d0c6f57a7e4f2a7a697c41
sanjay gupta <guptaji30749@gmail.com>
2022-07-23 16:32:07 +0530

    final

---
+++ script/index.js
@@ -0,0 +1,24 @@
+function getdata(e) {
+
+    e.preventDefault()
+
+    let form_data = document.getElementById("data")
+
+    let name = form_data.name.value
+
+    let last = form_data.Last.value
+
+    let email = form_data.email.value
+
+    let company = form_data.company.value
+
+    let job = form_data.job.value
+
+    let phn = form_data.phn.value
+
+    let india = form_data.india.value
+
+
+    // console.log(name, last, email, company, job, phn, india)
+    alert("Your enter successfully !")
+}
