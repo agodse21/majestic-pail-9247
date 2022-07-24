9c16d51f3ee8093accb65e131bd79df29f7e36b6
Sonu Prasad <sp.srp1999@gmail.com>
2022-07-24 10:34:22 +0545

    plan

--- plans.js
+++ plans.js
@@ -1,4 +1,9 @@
 
+import nav from "../component/nav.js"
+
+document.getElementById("navbar").innerHTML=nav();
+
+
 document.getElementById("need_more_btn").addEventListener("click",()=>{
     window.location.href="#additional"
 
